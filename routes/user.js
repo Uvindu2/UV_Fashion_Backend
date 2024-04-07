6dadf5bacb2614374c648774fe5d4fdacb3b618c
Uvindu Perera <uvinduperera97@gmail.com>
2024-04-07 17:54:35 +0530

    fix user id get by username

--- routes/user.js
+++ routes/user.js
@@ -33,5 +33,23 @@ router.post('/verify-password', async (req, res) => {
     }
 });
 
+router.get('/:username', (req, res) => {
+    const username = req.params.username;
+    console.log("Received username:", username); // Log username for troubleshooting
+    // Find the user with the given username
+    User.findOne({ username: username }, '_id')
+        .then((user) => {
+            console.log("Fetched user:", user); // Log fetched user for troubleshooting
+            if (user) {
+                // Return the user's ID
+                res.json({ userId: user._id });
+            } else {
+                res.status(404).json({ message: "User not found" });
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ message: error.message });
+        });
+});
 
 module.exports = router;
